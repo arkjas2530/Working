0a7bb44d3d9484af5069f29691a90142149d8c65
kulis95 <kulisdominik@gmail.com>
2016-06-05 14:30:49 +0200

    Update CTable.cpp

--- Test/CTable.cpp
+++ Test/CTable.cpp
@@ -67,6 +67,48 @@ void CTable::heapSort()
 void CTable::bubbleSortCOM()
 {}
 void CTable::bucketSort()
-{}
+{
+    // Ustawienie poczatkowych wartosci dla max i min
+    int min = table[0], max = table[0];
+   
+    // Szukanie minimum oraz maksimum
+    for(int i = 1; i < n; i++)
+    {
+        if(min > table[i]) min = table[i];
+        if(max < table[i]) max = table[i];
+    }
+   
+    // Wielkosc tablicy pomocniczej
+    int sizeTable = max - min + 1;
+   
+    // Tworzenie tablicy pomocniczej
+    int *tableHelper = new int[sizeTable];
+   
+    // Zerowanie licznikow tablicy
+    for(int i = 0; i < sizeTable; i++)
+        tableHelper[i] = 0;   
+
+    // Zliczanie licznikow
+    for(int i = 0; i < n; i++)
+        tableHelper[table[i] - min]++;
+ 
+   
+    // Wartosci niezerowych licznikow zapisz do table
+    // tyle razy ile wystepuja w tabe
+    for(int i = 0, j = 0; i < sizeTable; i++)
+        while(tableHelper[i]--)
+			table[j++] = i + min;
+   
+    for(int i = 0; i < n; i++)
+        std::cout << table[i] << ", ";
+
+	std::cout << std::endl << "Ilosc porownan elementow: " << n - 1 << std::endl; 
+	std::cout << "Ilosc przestawien elementow: " << n << std::endl; 
+
+	// W wolnej chwili niech ktos zobaczy czy to zliczanie porownan jest dobrze
+	// w tym sortowaniu nie nastepuje zadna zamiana miejsc a porownuje sie tylko max i min
+	// a ilosc przestawien to po prostu ilosc zapisow do tablicy pomocniczej 
+	
+}
 void CTable::quickSortL()
 {}
